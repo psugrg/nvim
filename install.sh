46fa85e2242280dcaeaf688c7b7bf8b496b246ee
Przemysław Sulecki <psugrg@gmail.com>
2020-11-27 22:02:25 +0100

    Install script added

---
+++ install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+# Install dependencies
+apt-get  update && \
+  apt-get upgrade -y && \
+  apt-get install -y \
+  software-properties-common \
+  curl \
+  git \
+  neovim \
+  ranger \
+  xsel \
+  python3 \
+  python3-pip \
+  fzf 
+
+add-apt-repository ppa:lazygit-team/release -y
+apt-get update && apt-get install lazygit
+
+pip3 install pynvim
+
+# Clone nvim configuration
+mkdir -p $HOME/.config
+pushd $HOME/.config/
+git clone https://github.com/psugrg/nvim.git
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+popd
