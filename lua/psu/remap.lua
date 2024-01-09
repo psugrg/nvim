21ce487d7b7df68e60d94b9c40bfa30e664326a0
Przemysław Sulecki <psugrg@gmail.com>
2024-01-09 12:20:08 +0100

    spawn terminal

--- lua/psu/remap.lua
+++ lua/psu/remap.lua
@@ -17,3 +17,6 @@ keymap("n", "<leader><leader>", ":Ex<CR>", options) -- Open files navigation
 keymap("n", "<C-s>", ":w<CR>", options) -- Save using Ctrl+s
 keymap("i", "<C-s>", ":w<CR>", options) -- Save using Ctrl+s
 
+-- Terminal
+keymap("n", "<leader>t", ":split<CR>:terminal<CR>i", options) -- Open files navigation
+keymap("t", "<Esc>", "<C-\\><C-n>", options) -- Use ESC to escape terminal mode
