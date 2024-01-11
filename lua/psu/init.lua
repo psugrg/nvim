e900780e9e0084937543ddb557d8721c1af1be73
Przemysław Sulecki <psugrg@gmail.com>
2024-01-11 11:12:24 +0100

    improving directory structure

--- lua/psu/init.lua
+++ lua/psu/init.lua
@@ -1,8 +1,11 @@
 require("psu.set")
 require("psu.remap")
 require("psu.lazy") -- plugin manager
+require("psu.plugin.telescope")
+require("psu.plugin.treesitter")
+require("psu.plugin.rose-pine")
 
--- Choose your color scheme by uncommenting one of these lines:
+-- Choome your color scheme by uncommenting one of these lines:
 vim.cmd('colorscheme rose-pine')
 -- vim.cmd('colorscheme rose-pine-main')
 -- vim.cmd('colorscheme rose-pine-moon')
