eaabd19e56aa9636f764f09a4e95e73c43112a76
Przemysław Sulecki <psugrg@gmail.com>
2024-01-11 09:55:08 +0100

    colorsheme selection

--- lua/psu/init.lua
+++ lua/psu/init.lua
@@ -1,3 +1,9 @@
 require("psu.set")
 require("psu.remap")
 require("psu.lazy") -- plugin manager
+
+-- Choose your color scheme by uncommenting one of these lines:
+vim.cmd('colorscheme rose-pine')
+-- vim.cmd('colorscheme rose-pine-main')
+-- vim.cmd('colorscheme rose-pine-moon')
+-- vim.cmd('colorscheme rose-pine-dawn')
