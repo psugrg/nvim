b8ac425b33932e6a7728dab3922dbdc08ac20a3b
Przemysław Sulecki <psugrg@gmail.com>
2024-02-13 20:44:06 +0100

    :bn remapped to Tab

--- lua/psu/remap.lua
+++ lua/psu/remap.lua
@@ -14,3 +14,7 @@ keymap("i", "kj", "<ESC>", options) -- Exit insert mmode using k+j
 -- File modification
 keymap("n", "<C-s>", ":w<CR>", options) -- Save using Ctrl+s
 keymap("i", "<C-s>", ":w<CR>", options) -- Save using Ctrl+s
+
+-- Bufer manipulation
+keymap("n", "<tab>", ":bn<CR>", options) -- Move to next buffer
+
