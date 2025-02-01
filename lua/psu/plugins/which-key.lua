c54ea225fe578f3cad722f491a79d368c21d8448
Przemyslaw Sulecki <psugrg@gmail.com>
2025-02-01 16:10:36 +0100

    which-key.lua configuration updated to the newest version

--- lua/psu/plugins/which-key.lua
+++ lua/psu/plugins/which-key.lua
@@ -14,17 +14,14 @@ return {
       local wk = require("which-key")
       local tb = require('telescope.builtin')
       -- Register custom commands in you own menu
-      wk.register({
-          e = { "<cmd>Ex<cr>", "File explorer" }, -- command assinged to a key
-          t = { "<cmd>terminal<cr><cmd>set nonu<cr>i", "Terminal" },
-          f = { -- key that has a sub-menu
-              name = "Find", -- optional group name
-              f = { function() tb.find_files() end, "Find files" }, -- command assigned to a key
-              g = { function() tb.git_files() end, "Find files in git repository" },
-              b = { function() tb.buffers() end, "Find buffer" },
-            }
-        }, 
-        { prefix = "<leader>" })
+      wk.add({
+          { "<leader>e", "<cmd>Ex<cr>", desc = "File explorer" },
+          { "<leader>t", "<cmd>terminal<cr><cmd>set nonu<cr>i", desc = "Terminal" },
+          { "<leader>f", group = "Find" },
+          { "<leader>ff", function() tb.find_files() end, desc = "Find files" },
+          { "<leader>fg", function() tb.git_files() end, desc = "Find files in git repository" },
+          { "<leader>fb", function() tb.buffers() end, desc = "Find buffer" },
+        })
   end
 }
 
