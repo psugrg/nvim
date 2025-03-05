f94a1e57064c23e63219a224d89f38192af9a606
Przemyslaw Sulecki <psugrg@gmail.com>
2025-03-05 22:12:12 +0100

    blade support fixed

--- lua/psu/plugins/treesitter.lua
+++ lua/psu/plugins/treesitter.lua
@@ -6,6 +6,23 @@ return {
 		-- import nvim-treesitter plugin
 		local treesitter = require("nvim-treesitter.configs")
 
+		-- configure blade
+		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		parser_config.blade = {
+			install_info = {
+				url = "https://github.com/EmranMR/tree-sitter-blade",
+				files = { "src/parser.c" },
+				branch = "main",
+			},
+			filetype = "phtml",
+		}
+
+		vim.filetype.add({
+			pattern = {
+				[".*%.blade%.php"] = "phtml",
+			},
+		})
+
 		-- configure treesitter
 		treesitter.setup({ -- enable syntax highlighting
 			highlight = {
@@ -50,15 +67,5 @@ return {
 				},
 			},
 		})
-
-		local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-		parser_config.blade = {
-			install_info = {
-				url = "https://github.com/EmranMR/tree-sitter-blade",
-				files = { "src/parser.c" },
-				branch = "main",
-			},
-			filetype = "blade",
-		}
 	end,
 }
